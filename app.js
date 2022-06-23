04e45d425baf13b052349b7ec761fcc5738d913f
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-06-24 04:22:20 +0600

    mvc architecture

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+app.use(express.json())
+app.use(cors())
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASSWORD;
+
+const uri = `mongodb+srv://${db_user}:${db_pass}@cluster0.tubwp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("mongodb connected")
+        const dataCollection = client.db("assignment11").collection("assignment11-data");
+
+
+        //   post 
+        app.post("/products", async (req, res) => {
+            const result = await dataCollection.insertOne(req.body)
+            res.send(result)
+        })
+
+
+        // count total data available 
+        app.get('/productCount', async (req, res) => {
+            const count = await dataCollection.countDocuments();
+            res.send({ count })
+        })
+
+
+        // home route
+        app.get('/', async(req, res) => {
+            await res.sendFile(__dirname+"/view/homeRoute.html");
+        })
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+module.exports= app;
